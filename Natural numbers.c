28898f8362d2e1e6c9003e6a1dcbbe9a58d36d63
26bst032 <26bst032@hicas.ac.in>
2026-09-08 07:34:01 +0530

    Create Natural numbers.c

---
+++ Natural numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int n,i,sum=0;
+printf("enter N:");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+  sum=sum+i;
+printf("Sum=%d",sum);
+return 0;
+}
+output:
+enter N:10
+  sum =5 5
