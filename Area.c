1918a3de944b07e5acd0adc652a17e36e287e898
26bst032 <26bst032@hicas.ac.in>
2026-09-08 07:28:36 +0530

    Create Area.c

---
+++ Area.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  float r,area;
+printf("enter radius:");
+scanf("%f",&n);
+area =3.14*r*r;
+printf("area of circle = %.2f",area);
+return 0;
+}
+output:
+enter radius:5
+  area of circle =78.50
